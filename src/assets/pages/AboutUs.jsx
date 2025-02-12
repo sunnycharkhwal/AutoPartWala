7e73ac6c5e15502d6f0b93fa77eb2c539e1dc9bf
Sunnny charkhwal <sunny.charkhwal@gmail.com>
2025-02-12 16:30:01 +0530

    about add 1000

--- src/assets/pages/AboutUs.jsx
+++ src/assets/pages/AboutUs.jsx
@@ -31,31 +31,33 @@ export const AboutUs = () => {
         </p>
         <p>Email: info@eauto.co.in</p>
       </div>
-      <div className="row mt-4">
-        <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 aboutus_infoOuter">
-          <div className="aboutus_info">
-            <img src={LiveChat} alt="icon" />
-            <div>
-              <p>Live Chat</p>
-              <span>Talk to an agent</span>
+      <div className="aboutus_info_div">
+        <div className="row ">
+          <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 aboutus_infoOuter">
+            <div className="aboutus_info">
+              <img src={LiveChat} alt="icon" />
+              <div>
+                <p>Live Chat</p>
+                <span>Talk to an agent</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 aboutus_infoOuter">
-          <div className="aboutus_info">
-            <img src={FastDelivery} alt="icon" />
-            <div>
-              <p>Fast Delivery</p>
-              <span>We deliver all over India</span>
+          <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 aboutus_infoOuter">
+            <div className="aboutus_info">
+              <img src={FastDelivery} alt="icon" />
+              <div>
+                <p>Fast Delivery</p>
+                <span>We deliver all over India</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 aboutus_infoOuter">
-          <div className="aboutus_info">
-            <img src={SecurePayments} alt="icon" />
-            <div>
-              <p>Secure payments</p>
-              <span>Secure and reliable payment always</span>
+          <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 aboutus_infoOuter">
+            <div className="aboutus_info">
+              <img src={SecurePayments} alt="icon" />
+              <div>
+                <p>Secure payments</p>
+                <span>Secure and reliable payment always</span>
+              </div>
             </div>
           </div>
         </div>
