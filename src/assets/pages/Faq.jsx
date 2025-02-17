3a6e1cca74a2cea94742e7208bc27a5ec3808151
Sunnny charkhwal <sunny.charkhwal@gmail.com>
2025-02-17 18:11:01 +0530

    faq

--- src/assets/pages/Faq.jsx
+++ src/assets/pages/Faq.jsx
@@ -6,44 +6,50 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
+import {UpperFooter} from '../component/UpperFooter';
 export const Faq = () => {
   return (
-    <div className="outerDiv">
-      <div className="FaqTitle">
-        <h3>FAQs (Frequently Asked Questions)</h3>
-      </div>
+    <>
+      <div className="outerDiv">
+        <div className="FaqTitle">
+          <h3>FAQs (Frequently Asked Questions)</h3>
+        </div>
 
-      <div className="FaqDiv">
-        <div className="FaqDiv_title">Shipping & Delivery</div>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id="panel1-header">
-            <Typography component="span">
-              What courier service do you use?
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel12-content"
-            id="panel2-header">
-            <Typography component="span">
-              What courier service do you use?
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </AccordionDetails>
-        </Accordion>
+        <div className="FaqDiv">
+          <div className="FaqDiv_title">Shipping & Delivery</div>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header">
+              <Typography component="span">
+                What courier service do you use?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel12-content"
+              id="panel2-header">
+              <Typography component="span">
+                What courier service do you use?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </AccordionDetails>
+          </Accordion>
+        </div>
       </div>
-    </div>
+      <UpperFooter />
+    </>
   );
 };
